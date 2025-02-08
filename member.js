a0e36067db4f696dd2a719b43b2095e53d994756
Sujit Jayaraj <sujitjayaraj05@gmail.com>
2025-02-08 13:20:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "Kobe Bryant",
+        age: 41,
+        skills: ["shoot", "dribble", "pass"],
+        team: "Lakers"
+    }
+    return member;
+}
